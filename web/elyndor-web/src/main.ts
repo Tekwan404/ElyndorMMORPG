f4b5f70462f817e39fbad8e6d51a97cb00aa42d5
Tekwan404 <46284118+Tekwan404@users.noreply.github.com>
2026-09-03 10:56:51 +0500

    style: load talent artwork presentation overrides

--- web/elyndor-web/src/main.ts
+++ web/elyndor-web/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import './styles/tokens.css'
 import './styles/base.scss'
+import './styles/talent-icons.scss'
 
 const app = createApp(App)
 
