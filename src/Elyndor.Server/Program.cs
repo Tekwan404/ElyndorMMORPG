ad0ae50dc7604dc58ec12d693f783f2f6c06bdbf
Tekwan404 <46284118+Tekwan404@users.noreply.github.com>
2026-09-09 01:22:50 +0500

    fix: scope Telegram admin update processor

--- src/Elyndor.Server/Program.cs
+++ src/Elyndor.Server/Program.cs
@@ -78,7 +78,7 @@ builder.Services.AddSingleton<TelegramInitDataValidator>();
 builder.Services.AddSingleton<JwtTokenIssuer>();
 builder.Services.AddSingleton(new HttpClient());
 builder.Services.AddSingleton<ITelegramMessageSender, TelegramBotMessageSender>();
-builder.Services.AddSingleton<TelegramAdminUpdateProcessor>();
+builder.Services.AddScoped<TelegramAdminUpdateProcessor>();
 builder.Services.AddSingleton<TelegramWebhookRegistrationService>();
 builder.Services.AddHostedService<TelegramWebhookRegistrationWorker>();
 builder.Services.AddHostedService<TelegramAdminLongPollingWorker>();
