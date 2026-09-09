04121f07573bc18227a7ffae94dab0499418dc0d
Tekwan404 <46284118+Tekwan404@users.noreply.github.com>
2026-09-09 10:06:32 +0500

    feat(itemization): map generated item metadata

--- src/Elyndor.Infrastructure/Persistence/Configurations/CharacterItemConfiguration.cs
+++ src/Elyndor.Infrastructure/Persistence/Configurations/CharacterItemConfiguration.cs
@@ -22,9 +22,35 @@ public sealed class CharacterItemConfiguration : IEntityTypeConfiguration<Charac
         builder.Property(item => item.RolledIntellect).HasPrecision(18, 4);
         builder.Property(item => item.RolledStamina).HasPrecision(18, 4);
         builder.Ignore(item => item.RolledPrimaryStats);
+        builder.Ignore(item => item.IsProcedurallyGenerated);
+        builder.Property(item => item.ItemLevel);
+        builder.Property(item => item.MinimumTemplateItemPower).HasPrecision(18, 4);
+        builder.Property(item => item.ActualItemPower).HasPrecision(18, 4);
+        builder.Property(item => item.MaxTemplateItemPower).HasPrecision(18, 4);
+        builder.Property(item => item.RollQuality).HasPrecision(7, 2);
+        builder.Property(item => item.Stars);
+        builder.Property(item => item.IsPerfect).HasDefaultValue(false).IsRequired();
+        builder.Property(item => item.PerfectOrigin).HasMaxLength(16);
+        builder.Property(item => item.GenerationVersion).HasDefaultValue(0).IsRequired();
+        builder.Property(item => item.GenerationSeedHash).HasMaxLength(64);
+        builder.Property(item => item.GeneratedPrefixId).HasMaxLength(128);
+        builder.Property(item => item.GeneratedSuffixId).HasMaxLength(128);
+        builder.Property(item => item.GeneratedDisplayName).HasMaxLength(256);
+        builder.Property(item => item.ReforgeSlotKey).HasMaxLength(64);
+        builder.Property(item => item.ReforgeCount).HasDefaultValue(0).IsRequired();
+        builder.Property(item => item.EnhancementLevel).HasDefaultValue(0).IsRequired();
+        builder.Property(item => item.SourceType).HasMaxLength(32);
+        builder.Property(item => item.SourceOperationId);
+        builder.Property(item => item.SourceEntryId).HasMaxLength(128);
         builder.Property(item => item.Quantity).IsRequired();
         builder.Property(item => item.AcquiredAtUtc).IsRequired();
         builder.Property(item => item.IsLocked).HasDefaultValue(false).IsRequired();
+
+        builder.HasMany(item => item.Affixes)
+            .WithOne()
+            .HasForeignKey(affix => affix.ItemInstanceId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .HasConstraintName("fk_character_item_affixes_character_items_item_id");
 
         builder.HasOne<Character>()
             .WithMany()
@@ -34,5 +60,7 @@ public sealed class CharacterItemConfiguration : IEntityTypeConfiguration<Charac
 
         builder.HasIndex(item => new { item.CharacterId, item.ItemDefinitionId })
             .HasDatabaseName("ix_character_items_character_definition");
+        builder.HasIndex(item => item.SourceOperationId)
+            .HasDatabaseName("ix_character_items_source_operation");
     }
 }
