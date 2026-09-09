83f412fbe0dcd61d0cf1c58efb9be48ff59b6b5c
tekwan404 <hasagi74@gmail.com>
2026-09-09 22:32:58 +0500

    test: update content version expectation

--- tests/Elyndor.IntegrationTests/Content/GameContentPackageLoaderTests.cs
+++ tests/Elyndor.IntegrationTests/Content/GameContentPackageLoaderTests.cs
@@ -21,7 +21,7 @@ public sealed class GameContentPackageLoaderTests
         GameContentPackage package = await GameContentPackageLoader.LoadAsync(
             Path.GetFullPath("content/package.json"));
 
-        Assert.Equal("0.17.0", package.ContentVersion);
+        Assert.Equal("0.18.0", package.ContentVersion);
         Assert.Equal("0.14.0", package.BalanceVersion);
         Assert.NotNull(package.LevelProgression);
         Assert.Contains(package.Items!, item => item.Id == "RECRUIT_IRON_SWORD");
