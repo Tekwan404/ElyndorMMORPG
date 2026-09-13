e358fbb32b4379f5b6e11fcd7ed85fd81275944b
Tekwan404 <46284118+Tekwan404@users.noreply.github.com>
2026-09-13 23:09:10 +0500

    Normalize generated SVG icons in combat UI

--- web/elyndor-web/src/main.ts
+++ web/elyndor-web/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import './styles/tokens.css'
 import './styles/base.scss'
+import './styles/combat-icons.css'
 
 const app = createApp(App)
 
