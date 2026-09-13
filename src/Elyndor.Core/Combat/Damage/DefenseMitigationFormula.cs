2ae8e52fa03017ef98d8ab72761844aaaa334158
Tekwan404 <46284118+Tekwan404@users.noreply.github.com>
2026-09-13 19:29:42 +0500

    balance: scale defense mitigation by attacker level

--- src/Elyndor.Core/Combat/Damage/DefenseMitigationFormula.cs
+++ src/Elyndor.Core/Combat/Damage/DefenseMitigationFormula.cs
@@ -2,14 +2,22 @@ namespace Elyndor.Core.Combat.Damage;
 
 public static class DefenseMitigationFormula
 {
-    public const decimal MitigationConstant = 100m;
+    public const decimal BaseMitigationConstant = 50m;
+    public const decimal MitigationConstantPerLevel = 40m;
+    public const decimal MaximumReductionPercent = 60m;
 
-    public static decimal CalculateDamageMultiplier(decimal defense)
+    public static decimal CalculateDamageMultiplier(decimal defense, int referenceLevel)
     {
+        ArgumentOutOfRangeException.ThrowIfLessThan(referenceLevel, 1);
+
         decimal effectiveDefense = Math.Max(0, defense);
-        return MitigationConstant / (MitigationConstant + effectiveDefense);
+        decimal mitigationConstant = BaseMitigationConstant
+            + (MitigationConstantPerLevel * referenceLevel);
+        decimal uncappedReduction = effectiveDefense / (mitigationConstant + effectiveDefense);
+        decimal cappedReduction = Math.Min(MaximumReductionPercent / 100m, uncappedReduction);
+        return 1m - cappedReduction;
     }
 
-    public static decimal CalculateReductionPercent(decimal defense) =>
-        (1 - CalculateDamageMultiplier(defense)) * 100m;
+    public static decimal CalculateReductionPercent(decimal defense, int referenceLevel) =>
+        (1 - CalculateDamageMultiplier(defense, referenceLevel)) * 100m;
 }
