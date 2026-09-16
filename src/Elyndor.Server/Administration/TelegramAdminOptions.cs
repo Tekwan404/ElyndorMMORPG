1e54d4df7b07ca1b75e36ff7d3b18425dd35dbe3
Tekwan404 <46284118+Tekwan404@users.noreply.github.com>
2026-09-16 16:44:35 +0500

    feat(monitoring): configure Telegram monitoring chat and interval

--- src/Elyndor.Server/Administration/TelegramAdminOptions.cs
+++ src/Elyndor.Server/Administration/TelegramAdminOptions.cs
@@ -16,7 +16,25 @@ public sealed class TelegramAdminOptions
 
     public bool UseLongPolling { get; init; }
 
+    public long ChatId { get; init; }
+
     public long[] AllowedUserIds { get; init; } = [];
+
+    public bool MonitoringEnabled { get; init; }
+
+    public int ReportIntervalMinutes { get; init; } = 15;
+
+    public int CpuWarningPercent { get; init; } = 80;
+
+    public int CpuCriticalPercent { get; init; } = 95;
+
+    public int MemoryWarningPercent { get; init; } = 80;
+
+    public int MemoryCriticalPercent { get; init; } = 95;
+
+    public int DiskWarningPercent { get; init; } = 80;
+
+    public int DiskCriticalPercent { get; init; } = 95;
 
     public bool IsConfigured =>
         AllowedUserIds.All(id => id > 0)
@@ -26,9 +44,25 @@ public sealed class TelegramAdminOptions
                     || (WebhookSecret.Length >= 32
                         && (!RegisterWebhookOnStartup || TryGetWebhookUri(out _))))));
 
+    public bool IsMonitoringConfigured =>
+        ChatId != 0
+        && ReportIntervalMinutes is >= 1 and <= 1440
+        && CpuWarningPercent is >= 1 and <= 100
+        && CpuCriticalPercent is >= 1 and <= 100
+        && CpuWarningPercent <= CpuCriticalPercent
+        && MemoryWarningPercent is >= 1 and <= 100
+        && MemoryCriticalPercent is >= 1 and <= 100
+        && MemoryWarningPercent <= MemoryCriticalPercent
+        && DiskWarningPercent is >= 1 and <= 100
+        && DiskCriticalPercent is >= 1 and <= 100
+        && DiskWarningPercent <= DiskCriticalPercent;
+
     public bool IsAllowedUser(long telegramUserId) =>
         telegramUserId > 0
         && AllowedUserIds.Contains(telegramUserId);
+
+    public bool IsAllowedChat(long telegramChatId) =>
+        ChatId != 0 && telegramChatId == ChatId;
 
     public bool TryGetWebhookUri(out Uri? webhookUri)
     {
