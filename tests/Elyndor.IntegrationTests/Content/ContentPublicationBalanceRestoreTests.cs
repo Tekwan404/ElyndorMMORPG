4a0b2673409d201e66cd47ad1eb8503deb2cc3a6
tekwan404 <hasagi74@gmail.com>
2026-09-17 18:41:11 +0500

    test: align world travel with authored level gates

--- tests/Elyndor.IntegrationTests/Content/ContentPublicationBalanceRestoreTests.cs
+++ tests/Elyndor.IntegrationTests/Content/ContentPublicationBalanceRestoreTests.cs
@@ -20,7 +20,7 @@ public sealed class ContentPublicationBalanceRestoreTests(PostgresFixture postgr
         GameContentPackage bundled = await GameContentPackageLoader.LoadAsync(
             Path.GetFullPath("content/package.json"));
         Assert.Equal("0.23.0", bundled.ContentVersion);
-        Assert.Equal("0.18.0", bundled.BalanceVersion);
+        Assert.Equal("0.19.0", bundled.BalanceVersion);
 
         var currentPotion = bundled.Items!
             .Single(item => item.Id == "SMALL_HEALING_POTION");
