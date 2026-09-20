40f2bd62ef1c53a3a913192aa22395b9c034f929
Tekwan404 <46284118+Tekwan404@users.noreply.github.com>
2026-09-20 12:27:14 +0500

    test: cover legacy inventory capacity migration

--- tests/Elyndor.UnitTests/Items/InventoryCapacityTests.cs
+++ tests/Elyndor.UnitTests/Items/InventoryCapacityTests.cs
@@ -1,3 +1,4 @@
+using Elyndor.Core.Content;
 using Elyndor.Infrastructure.Items;
 
 namespace Elyndor.UnitTests.Items;
@@ -9,4 +10,31 @@ public sealed class InventoryCapacityTests
     {
         Assert.Equal(100, InventoryCapacity.DefaultCapacity);
     }
+
+    [Fact]
+    public void LegacyFortySlotProfileMigratesToOneHundredSlots()
+    {
+        GameContentPackage package = CreatePackage(new InventoryProfileDefinition(40));
+
+        Assert.Equal(100, InventoryCapacity.Resolve(package));
+    }
+
+    [Theory]
+    [InlineData(80)]
+    [InlineData(120)]
+    public void ExplicitNonLegacyCapacityIsPreserved(int configuredCapacity)
+    {
+        GameContentPackage package = CreatePackage(new InventoryProfileDefinition(configuredCapacity));
+
+        Assert.Equal(configuredCapacity, InventoryCapacity.Resolve(package));
+    }
+
+    private static GameContentPackage CreatePackage(InventoryProfileDefinition? inventoryProfile) =>
+        new(
+            "test-content",
+            "test-balance",
+            DateTimeOffset.UnixEpoch,
+            [],
+            [],
+            InventoryProfile: inventoryProfile);
 }
